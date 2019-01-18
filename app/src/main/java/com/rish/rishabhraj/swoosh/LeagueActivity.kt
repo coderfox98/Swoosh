3abad5707050ecfd1a5f3473ab74d4c1149874c1
coderfox98 <coderfox98@gmail.com>
2019-01-18 21:00:28 +0530

    third activity functionality done

--- app/src/main/java/com/rish/rishabhraj/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/rish/rishabhraj/swoosh/LeagueActivity.kt
@@ -1,12 +1,21 @@
 package com.rish.rishabhraj.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
+
+    fun leagueNextClicked(view: View) {
+        val skillActivity = Intent(this,SkillActivity::class.java)
+        startActivity(skillActivity)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+
     }
 }
